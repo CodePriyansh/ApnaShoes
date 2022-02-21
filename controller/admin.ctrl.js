946b580994e8412c0c7cdf326840cbabc2dc9313
Your Name <you@example.com>
2022-02-21 16:47:28 +0530

    admin-login

--- controller/admin.ctrl.js
+++ controller/admin.ctrl.js
@@ -1,9 +1,24 @@
+const { required } = require("nodemon/lib/config");
+const Admin = require('../model/admin.js')
 
+exports.adminLoginPage = (req, res) => {
+  res.render("../Views/Admin-Pages/admin_login.ejs");
+};
 
-exports.adminHomePage=(req,res)=>{
-    res.render("./Admin-Pages/admin-home.ejs");
-}
-
-exports.adminLoginPage=(req,res)=>{
-    res.render("./Admin-Pages/admin_login.ejs");
-}
+exports.adminLoginPost = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let admin = new Admin(email,password);
+  admin
+    .checkAdmin()
+    .then((results) => {
+        console.log(results);
+      if (results.length > 0) {
+        //    req.session.current_user = email;
+        res.render("../Views/Admin-Pages/admin-home.ejs");
+      } else console.log("Login Failed...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const control = require('../controller/admin.ctrl')
+const control = require("../controller/admin.ctrl");
 
-router.get('/home',control.adminHomePage);
-router.get('/',control.adminLoginPage);
+// router.get('/home',control.adminHomePage);
+router.get("/", control.adminLoginPage);
+router.post("/login", control.adminLoginPost);
+
+// router.get("/dashboard",auth.isAuth,adminController.adminDashBoard);
 
 module.exports = router;
